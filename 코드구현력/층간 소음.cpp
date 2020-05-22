1d455c47e951434648753237f80c2b7505cffc36
joyfulSong <shy4797@gmail.com>
2020-05-22 17:58:17 +0200

    층간 소음
    값이 바뀔 때 어떻게 연속값을 counting하는가. max를 이용해 크기비교를 하자.

---
+++ 코드구현력/층간 소음.cpp
@@ -0,0 +1,25 @@
+// "최대 연속" 경보음 발생 초를 출력해야함. max를 통해서 하자.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int N, M, tmp, cnt = 0, max = -2147000000;
+	cin >> N >> M; // N초, 기준값 받기
+
+	for (int i = 0; i < N; i++) {
+		cin >> tmp; // 현재 초에서의 측정값 
+		if (tmp > M) // 측정값이 기준값보다 크면
+		{
+			cnt++;
+			if (cnt > max) max = cnt;  
+			//기존의 max보다 cnt가 크면 (즉 0으로 초기화 되지 않고 cnt가 연속이었다면)
+			// 그 cnt를 담아준다.
+		}
+		else cnt = 0; //측정값이 기준값 이하이면 cnt 초기화해버림
+	}
+	if (max == 0) cout << "-1" << "\n"; // max는 else의 cnt를 통해 0으로 한번이라도 초기화 되므로, 한번도 기준값 초과가 되지 않아도 0인 상태.
+	else cout << max << "\n";
+
+	return 0;
+}
