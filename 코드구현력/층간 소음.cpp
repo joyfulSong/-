3e911909fa6681ed49f80ead17879bc6c2e116b6
joyfulSong <45616059+joyfulSong@users.noreply.github.com>
2022-05-29 12:37:39 +0900

    .

--- 코드구현력/층간 소음.cpp
+++ 코드구현력/층간 소음.cpp
@@ -19,7 +19,7 @@ int main()
 		else cnt = 0; //측정값이 기준값 이하이면 cnt 초기화해버림
 	}
 	if (max == 0) cout << "-1" << "\n"; // max는 else의 cnt를 통해 0으로 한번이라도 초기화 되므로, 한번도 기준값 초과가 되지 않아도 0인 상태.
-	else cout << max << "\n";
+	else cout << cnt << "\n";
 
 	return 0;
 }
